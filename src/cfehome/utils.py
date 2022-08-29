538c79a0d897a38da8729d63ad7a1e26efbc69c6
CFE <hello@teamcfe.com>
2022-08-29 13:01:04 -0500

    6 - Generate Fake User Data

---
+++ src/cfehome/utils.py
@@ -0,0 +1,19 @@
+from faker import Faker
+
+
+def get_fake_profiles(count=10):
+    fake = Faker()
+    user_data = []
+    for _ in range(count):
+        profile = fake.profile()
+        data = {
+            "username": profile.get('username'),
+            "email": profile.get('mail'),
+            "is_active": True
+        }
+        if 'name' in profile:
+            fname, lname = profile.get('name').split(" ")[:2]
+            data['first_name'] = fname
+            data['last_name'] = lname
+        user_data.append(data)
+    return user_data
