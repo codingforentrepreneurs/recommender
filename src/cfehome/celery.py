3430850659225692fd3f8e2ead478823ac111eab
CFE <hello@teamcfe.com>
2022-08-30 10:41:42 -0500

    15 - Setup Celery for Offloading Tasks

--- src/cfehome/celery.py
+++ src/cfehome/celery.py
@@ -9,4 +9,6 @@ app = Celery('cfehome')
 # CELERY_
 app.config_from_object("django.conf:settings", namespace='CELERY')
 
+# app.conf.broker_url = ''
+# app.conf.result_backend ='django-db'
 app.autodiscover_tasks()
