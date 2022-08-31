ac4713ab58bf7b86bd4c84719fb43b108bffba05
CFE <hello@teamcfe.com>
2022-08-31 13:01:15 -0500

    23 - Rate Movies Dynamically with HTMX

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from ratings import views as ratings_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('movies/', include('movies.urls')),
+    path('rate/movie/', ratings_views.rate_movie_view)
 ]
--- src/ratings/views.py
+++ src/ratings/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.contenttypes.models import ContentType
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+
+from .models import Rating
+
+@require_http_methods(['POST'])
+def rate_movie_view(request):
+    if not request.htmx:
+        return HttpResponse("Not Allowed", status=400)
+    object_id = request.POST.get('object_id')
+    rating_value = request.POST.get("rating_value")
+    user = request.user
+    message = "You must <a href='/accounts/login'>login</a> to rate this."
+    if user.is_authenticated:
+        message = "<span class='bg-danger text-light py-1 px-3 rounded'>An error occured.</div>"
+        ctype = ContentType.objects.get(app_label='movies', model='movie')
+        rating_obj = Rating.objects.create(content_type=ctype, object_id=object_id, value=rating_value, user=user)
+        if rating_obj.content_object is not None:
+            message = "<span class='bg-success text-light py-1 px-3 rounded'>Rating saved!</div>"
+    return HttpResponse(message, status=200)
